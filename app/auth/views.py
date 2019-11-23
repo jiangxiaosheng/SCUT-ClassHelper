48100cabf5b63ad438dfbd39b5973ff74b7e6dd7
js <437822838@qq.com>
2019-11-23 11:18:34 +0800

    2019-11-21

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,7 +15,7 @@ def login():
         user = User.query.filter_by(email=form.email.data.lower()).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(url_for('main.index'))
+            return redirect(url_for('course.index'))
         flash('邮箱地址或密码错误')
     return render_template('auth/login.html', form=form)
 
